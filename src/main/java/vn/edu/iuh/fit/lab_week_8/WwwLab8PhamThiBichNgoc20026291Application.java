869d036da39ef1eb5d7777643682754564f16ec6
N-Rain <bngoc1792@gmail.com>
2023-11-25 09:21:48 +0700

    create project

---
+++ src/main/java/vn/edu/iuh/fit/lab_week_8/WwwLab8PhamThiBichNgoc20026291Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.lab_week_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwLab8PhamThiBichNgoc20026291Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwLab8PhamThiBichNgoc20026291Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/lab_week_8/WwwLab8PhamThiBichNgoc20026291ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.lab_week_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwLab8PhamThiBichNgoc20026291ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
